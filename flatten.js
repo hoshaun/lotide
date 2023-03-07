6b8db12194222feb5b86168706364b87eed1357d
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-07 13:50:03 -0500

    submit flatten.js

---
+++ flatten.js
@@ -0,0 +1,32 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const flatten = function(arr) {
+  let result = [];
+  for (const e of arr) {
+    if (Array.isArray(e)) {
+      for (const i of e) {
+        result.push(i);
+      }
+    } else {
+      result.push(e);
+    }
+  }
+  return result;
+};
+
+console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])); // => [1, 2, 3, 4, 5, 6]
