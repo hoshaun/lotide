f4540c4d36b78751c9fb8236f96bae0c994575cd
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-08 13:14:04 -0500

    submit middle.js

---
+++ middle.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const middle = function(arr) {
+  const midIndex = Math.ceil(arr.length / 2) - 1;
+  if (arr.length <= 2) {
+    return [];
+  } else if (arr.length % 2 === 0) {
+    return arr.slice(midIndex, midIndex + 1);
+  } else {
+    return arr[midIndex];
+  }
+};
+
+console.log(assertArraysEqual(middle([1]), []));
+console.log(assertArraysEqual(middle([1, 2]), []));
+console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
