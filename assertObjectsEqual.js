59f644ace1f936e20b1e45d5bdf285a4cc0eedd9
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-08 15:29:13 -0500

    submit

---
+++ assertObjectsEqual.js
@@ -0,0 +1,33 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (const i in object1) {
+    if (!eqArrays(object1[i], object2[i])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+assertObjectsEqual({a: '1', b: 2}, {b: 2, a: '1'});
