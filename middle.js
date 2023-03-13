3c0a7b0bb334c98ca2bebbc45457c428e3357f2c
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-13 13:21:47 -0400

    refactor middle

--- middle.js
+++ middle.js
@@ -1,43 +1,12 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  if (arr1.length === 0) {
-    return true;
-  }
-
-  if (!Array.isArray(arr1[0]) && !Array.isArray(arr2[0])) {
-    return arr1[0] === arr2[0] && eqArrays(arr1.slice(1), arr2.slice(1));
-  } else if (Array.isArray(arr1[0]) && Array.isArray(arr2[0])) {
-    return eqArrays(arr1[0], arr2[0]) && eqArrays(arr1.slice(1), arr2.slice(1));
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-};
-
 const middle = function(arr) {
   const midIndex = Math.ceil(arr.length / 2) - 1;
   if (arr.length <= 2) {
     return [];
   } else if (arr.length % 2 === 0) {
-    return arr.slice(midIndex, midIndex + 1);
+    return arr.slice(midIndex, midIndex + 2);
   } else {
-    return arr[midIndex];
+    return arr.slice(midIndex, midIndex + 1);
   }
 };
 
-console.log(assertArraysEqual(middle([1]), []));
-console.log(assertArraysEqual(middle([1, 2]), []));
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+module.exports = middle;
